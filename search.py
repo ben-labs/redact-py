080ab41712618682b5ad366775879dd3298e44f7
Ben <ben.quek@ben-labs.net>
2021-11-03 21:14:54 +0800

    Created search module

---
+++ search.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+Utility to search for files with contents to be redacted
+'''
+import argparse
+import sys
+import os
+
+from redactor.redactor import Search
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',help="path to scan")
+    parser.add_argument('-r', '--rulefile', help='Ruleset file')
+    parser.add_argument('-o', '--outpath', help="Output file path")
+    args = parser.parse_args()
+
+    if os.path.isdir(args.path):
+        if args.rulefile:
+            srch = Search(args.rulefile)
+        else:
+            srch = Search()
+
+        if args.outpath:
+            srch.execute(args.path, args.outpath)
+        else:
+            srch.execute(args.path)
