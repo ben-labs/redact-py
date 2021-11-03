73f7fe60e14d8b6ee0f441115df5536951845581
Ben <ben.quek@ben-labs.net>
2021-11-03 21:15:54 +0800

    Modified redactor. Added Search subclass

--- redactor/redactor.py
+++ redactor/redactor.py
@@ -22,7 +22,7 @@ class Redactor:
         Returns:
             None
         '''
-        self.allowed_files = [
+        self.__allowed_files__ = [
             'text/plain',
             'text/x-python',
             'application/json',
@@ -62,7 +62,7 @@ class Redactor:
         Returns:
             allowed_file (list): List of allowed files
         '''
-        return self.allowed_files
+        return self.__allowed_files__
 
     def allowed_file(self, file):
         '''Checks if supplied file is allowed
@@ -100,14 +100,14 @@ class Redactor:
         except json.JSONDecodeError:
             sys.exit("[ - ] Issue decoding rule file")
 
-    def redact_file(self, filename, filepath='./'):
+    def execute(self, filename, savepath='./'):
         '''Main redact function
 
         Main function to redact a file
 
         Args:
             filename (str): File to redact
-            filepath (str): [Optional] filepath to place results
+            savepath (str): [Optional] filepath to place results
 
         Returns:
             None
@@ -119,21 +119,21 @@ class Redactor:
             # Open a file read pointer fP
             with open(filename, encoding="utf-8") as fP:
                 # Check the format of the output directory
-                if filepath != './' and filepath[-1] != '/':
-                    filepath = filepath + '/'
+                if savepath != './' and savepath[-1] != '/':
+                    savepath = savepath + '/'
 
                 # created the directory if not there
-                if not os.path.exists(os.path.dirname(filepath)):
-                    print("[ + ] " + os.path.dirname(filepath) +
+                if not os.path.exists(os.path.dirname(savepath)):
+                    print("[ + ] " + os.path.dirname(savepath) +
                           " directory does not exist... Creating it")
-                    os.makedirs(os.path.dirname(filepath))
+                    os.makedirs(os.path.dirname(savepath))
 
                 print("[ + ] Processing starts now. This may take some time "
                       "depending on the file size. Monitor the redacted file "
                       "size to monitor progress")
 
                 # Open a file write pointer w
-                with open(f"{filepath}redacted_{os.path.basename(filename)}",
+                with open(f"{savepath}redacted_{os.path.basename(filename)}",
                           'w',
                           encoding="utf-8") as w:
                     for line in fP:
@@ -153,6 +153,76 @@ class Redactor:
             print(f'[ + ] Took {tt} seconds to execute')
         except UnicodeDecodeError:
             # remove the created file
-            os.remove(f"{filepath}redacted_{os.path.basename(filename)}")
+            os.remove(f"{savepath}redacted_{os.path.basename(filename)}")
             print("[ - ] Removed incomplete redact file")
             sys.exit("[ - ] Unable to read file")
+
+
+class Search(Redactor):
+    '''Search Class
+    
+    Class that is a subclass of Redactor.
+    Used to search for files that have items found in the rule files
+    '''
+    def execute(self, path, savepath='./'):
+        '''Search function
+
+        Main function to search a path for files that match what is stored in the rule file
+
+        Args:
+            filename (str): File to redact
+            savepath (str): [Optional] filepath to place results
+
+        Returns:
+            None
+        '''
+
+        count = 0
+        redact_count = 0
+        start = time.time()
+
+        for dirpath, dirnames, filenames in os.walk(path):
+            for name in filenames:                
+                if self.allowed_file(os.path.join(dirpath, name)):
+                    filename = os.path.join(dirpath, name)
+                    try:
+                        # Open a file read pointer fP
+                        with open(filename, encoding="utf-8") as fP:
+                            # Check the format of the output directory
+                            if savepath != './' and savepath[-1] != '/':
+                                savepath = savepath + '/'
+
+                            # created the directory if not there
+                            if not os.path.exists(os.path.dirname(savepath)):
+                                print("[ + ] " + os.path.dirname(savepath) +
+                                    " directory does not exist... Creating it")
+                                os.makedirs(os.path.dirname(savepath))
+
+                            print(f"[ + ] Processing {name} now.")
+                            
+                            linepos = 0
+                            for line in fP:
+                                linepos = linepos + 1
+                                for p in self.patterns:
+                                    if re.search(p['pattern'], line, re.IGNORECASE):
+                                        # Open a file write pointer w
+                                        with open(f"{savepath}found_{os.path.basename(filename)}",
+                                                'a',
+                                                encoding="utf-8") as w:
+                                            w.write(f'[ Line {str(linepos).rjust(10)} ]   : {line}')
+                                        redact_count = redact_count + 1
+
+                                count = count + 1
+                    except UnicodeDecodeError:
+                        # remove the created file
+                        os.remove(f"{savepath}found_{os.path.basename(filename)}")
+                        print("[ - ] Removed incomplete redact file")
+                        sys.exit("[ - ] Unable to read file")
+
+
+        end = time.time()
+        print()
+        print(f"[ + ] Processed {count} records...")
+        print(f"[ + ] Found {redact_count} targets...")
+        tt = end - start
+        print(f'[ + ] Took {tt} seconds to execute')
