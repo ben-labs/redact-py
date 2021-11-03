fb26175ad492845eef7a286a2355661fc6f0e66a
Ben <ben.quek@ben-labs.net>
2021-11-03 16:14:42 +0800

    Added google style docstrings

--- redactor/redactor.py
+++ redactor/redactor.py
@@ -7,7 +7,17 @@ import re
 
 
 class Redactor:
+    ''' Redactor class - Class containing all methods to support redaction of sensitive data '''
+
     def __init__(self, rulefile=None) -> None:
+        '''Class Initialization
+
+        Args:
+            rulefile (str): Rulefile name
+
+        Returns:
+            None
+        '''
         self.__load_allowed_files__()
         if rulefile is None:
             self.__read_rules__()
@@ -15,25 +25,71 @@ class Redactor:
             self.__read_rules__(rulefile)
 
     def check_file_type(self, file):
+        '''Checks for the supplied file type
+        
+        Args:
+            file (str): Filename of file to check
+
+        Returns:
+            mime (str): Mime type
+        '''
         if not os.path.isfile(file):
             return (None, None)
         return mimetypes.guess_type(file)[0]
 
     def __load_allowed_files__(self):
+        '''Helper function to load pre-defined allowed files
+
+        Sets allowed_files with list of allowed file types read
+        from allowed_types.dat
+
+        Args: 
+            None
+
+        Returns:
+            None
+        '''
         with open('redactor/allowed_types.dat', encoding="utf-8") as fp:
             self.allowed_files = fp.read().split('\n')
 
     def get_allowed_files(self):
+        '''Gets a list of allowed files
+
+        Args: 
+            None
+
+        Returns:
+            allowed_file (list): List of allowed files
+        '''
         return self.allowed_files
 
     def allowed_file(self, file):
+        '''Checks if supplied file is allowed
+
+        Checks the supplied file to see if it is in the allowed_files list
+
+        Args:
+            file (str): File to check
+
+        Returns:
+            False: File not found / File type is not allowed
+            True: File is allowed
+        '''
         if not os.path.isfile(file):
             return False
         return mimetypes.guess_type(file)[0] in self.get_allowed_files()
 
     def __read_rules__(self, ruleFile='default_rules.conf'):
-        '''
-        Read a json rule file and return a json object
+        '''Load Rules
+
+        Loads either a default ruleset or a self defined ruleset.
+        Rules are loaded to patterns variable
+
+        Args:
+            ruleFile (str): Custom rule file to be loaded
+        
+        Returns:
+            None
         '''
         try:
             with open(ruleFile) as fP:
@@ -44,8 +100,16 @@ class Redactor:
             sys.exit("[ - ] Issue decoding rule file")
 
     def redact_file(self, filename, filepath='./'):
-        '''
-        Redact file
+        '''Main redact function
+
+        Main function to redact a file
+
+        Args:
+            filename (str): File to redact
+            filepath (str): [Optional] filepath to place results
+
+        Returns:
+            None
         '''
         count = 0
         redact_count = 0
